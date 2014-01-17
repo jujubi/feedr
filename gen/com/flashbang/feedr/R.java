f67ed2f3f52cbb805e2eb7bd89f4b4478e1f2f03
unknown <suhas.grama@SDW1080.spanservices.com>
2014-01-17 13:46:48 +0530

    Added description and image

--- gen/com/flashbang/feedr/R.java
+++ gen/com/flashbang/feedr/R.java
@@ -27,12 +27,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int ipatch=0x7f020001;
         public static final int list_item_selector=0x7f020002;
-        public static final int rowshadow=0x7f020003;
+        public static final int mseye=0x7f020003;
+        public static final int rowshadow=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int list=0x7f090002;
-        public static final int progressBar=0x7f090001;
+        public static final int action_settings=0x7f090005;
+        public static final int imageView1=0x7f090001;
+        public static final int list=0x7f090004;
+        public static final int progressBar=0x7f090003;
+        public static final int textView1=0x7f090002;
         public static final int title=0x7f090000;
     }
     public static final class layout {
